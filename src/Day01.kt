77e7024821e56ac1218bfee9d02448b76be6e831
asm0dey <pavel.finkelshtein@gmail.com>
2022-12-01 08:40:11 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,29 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun countElfCalories(input: List<String>): ArrayList<Int> {
+        val results = arrayListOf<Int>()
+        var current = 0
+        for (s in input) {
+            if (s.isBlank()) {
+                results.add(current)
+                current = 0
+            } else {
+                current += s.toInt()
+            }
+        }
+        return results
     }
+
+    fun part1(input: List<String>): Int {
+        return countElfCalories(input).max()
+    }
+
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return countElfCalories(input).sortedDescending().take(3).sum()
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
