d3e4705f723165a4ab80c37359eb93c442a93c19
asm0dey <pavel.finkelshtein@gmail.com>
2022-12-03 11:07:00 +0100

    Day 3

---
+++ src/Day03.kt
@@ -0,0 +1,33 @@
+fun main() {
+
+    fun letterToCode(it: Char) = if (it.isUpperCase()) it.code - 38 else it.code - 96
+    fun part1(input: List<String>): Int {
+        return input
+            .asSequence()
+            .filterNot(String::isBlank)
+            .map(String::toList)
+            .map { it.subList(0, it.size / 2).toSet().intersect(it.subList(it.size / 2, it.size).toSet()) }
+            .filter { it.size == 1 }
+            .map { it.single() }
+            .sumOf(::letterToCode)
+    }
+
+    fun part2(input: List<String>): Int {
+        return input
+            .asSequence()
+            .filterNot(String::isBlank)
+            .chunked(3) {
+                it.map(String::toSet).reduce { acc, next -> acc.intersect(next) }
+            }
+            .filter { it.size == 1 }
+            .map { it.single() }
+            .sumOf(::letterToCode)
+    }
+
+    val testInput = readInput("Day03_test")
+    check(part1(testInput) == 157)
+
+    val input = readInput("Day03")
+    println(part1(input))
+    println(part2(input))
+}
